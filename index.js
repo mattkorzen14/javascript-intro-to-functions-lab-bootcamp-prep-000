f9d97f64eb059c1391a5e8d2194e1a952c193aab
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-09 19:13:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function whisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return "I can't hear you" 
+    return "I can't hear you!" 
   }
   else if(string === string.toUpperCase()) {
     return "YES INDEED!" 
